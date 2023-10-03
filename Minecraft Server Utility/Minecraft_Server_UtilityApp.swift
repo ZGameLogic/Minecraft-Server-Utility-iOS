6a82cbc1e136833f555a73159a5af2a960e71240
Ben Shabowski <benshabowski@gmail.com>
2023-10-03 16:18:59 -0500

    initial commit

---
+++ Minecraft Server Utility/Minecraft_Server_UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minecraft_Server_UtilityApp.swift
+//  Minecraft Server Utility
+//
+//  Created by Benjamin Shabowski on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Minecraft_Server_UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
